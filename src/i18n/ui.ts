2dfb61b43bd536db3b0cffecf26a3bbac260f676
SzymonPucher <sajmontube+github@gmail.com>
2026-01-06 19:33:02 +0100

    feat: improve how header is displayed on smaller screens

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,12 +10,16 @@ export const ui = {
     'nav.home': 'About',
     'nav.projects': 'Projects',
     'nav.blog': 'Blog',
+    'nav.contact': 'Contact',
     'nav.twitter': 'Twitter',
+    'nav.language': 'Language',
   },
   pl: {
     'nav.home': 'O mnie',
     'nav.projects': 'Projekty',
     'nav.blog': 'Blog',
+    'nav.contact': 'Kontakt',
     'nav.twitter': 'Twitter',
+    'nav.language': 'Language',
   },
 } as const;
